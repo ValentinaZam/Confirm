cc8474ec77754b3adb5a856816d63215d2e8f6be
ValentinaZam <109306246+ValentinaZam@users.noreply.github.com>
2022-11-18 01:14:24 +0300

    commit_confirm
    commit_confirm

---
+++ confirm/index.js
@@ -0,0 +1,22 @@
+let jsDateOfCreation = confirm('JavaScript появился в 1995 году?');
+if (jsDateOfCreation == true){
+    alert('Верно')
+}else{
+    alert('А ведь JavaScript появился в 1995 году!')
+};
+
+let jsSpecification = confirm('Спецификация JavaScript называется ECMAScript?');
+if (jsSpecification == true){
+    alert('Верно')
+}else{
+    alert('А ведь спецификация JavaScript называется ECMAScript!')
+};
+
+let jsTimeToCreate = confirm('JavaScript был создан за 1 месяц?');
+if (jsTimeToCreate == true){
+    alert('Верно')
+}else{
+    alert('JavaScript действительно был создан за 1 месяц')
+};
+
+
